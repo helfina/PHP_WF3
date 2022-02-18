f5d512923409b4011da0e3149cc3b40d6334824f
gaelle kerforne <stage.seeweb@gmail.com>
2022-02-18 11:57:40 +0100

    corriger exo 3
    Took 18 seconds

--- 04-Exercices/correctionExo3.php
+++ 04-Exercices/correctionExo3.php
@@ -1,34 +1,73 @@
+
 <?php
-// 3 - Créer un fichier formulaire.php et réaliser un formulaire permettant de selectionner (select) un fruit et saisir un poids.
-// -> Affichez via la fonction calcul(), le resultat issue des infos du formulaire
-// -> bonus : faites en sorte de garder le dernier fruit sélectionné et le dernier poids saisie dans le formulaire lorsque celui-ci est validé.
 
-print '<pre>';
-print_r( $_POST );
-print '</pre>';
+require_once "fonction.inc.php";//inclusion du fichier 'header.inc.php' qui est dans un dossier 'inc'
+//echo $_POST['fruit'];
 
-include "fonction.inc.php"; //Inclusion de la fonction
+// ici c'est la condition pour le champ de poids saisi. Cette condition est lié au  code php de la ligne 45 
+//<input type="text" name="poids" id="poids" value="<?php echo $poids_saisi; 
+////l'objective est de afficher le dernier choix saisi pour le dernier utilisateur
 
-if( $_POST ){ //Si validation du formulaire
-
-    echo calcul( $_POST['fruit'], $_POST['poids'] );
+if( !empty($_POST['poids']) ) {
+    $poids_saisi = $_POST['poids'];
+}
+else {
+    $poids_saisi = '';
+}
+//ici c'est la condition pour le champs fruit saisi. Cette condition est lié au code php de la ligne 39 au 42
+// <?php if($fruit_saisi == 'pommes') echo 'selected';
+//l'objective est de afficher le dernier choix saisi pour le dernier utilisateur
+if( !empty($_POST['fruit']) ) {
+    $fruit_saisi = $_POST['fruit'];
+}
+else {
+    $fruit_saisi = '';
 }
 
 ?>
-<hr>
-<form method="post">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lienPHP</title>
+</head>
+<body>
 
-    <label>Fruit</label><br>
-    <select name="fruit" >
-        <option value="pommes">Pomme</option>
-        <option value="cerises">Cerise</option>
-        <option value="bananes">Banane</option>
-        <option value="poires">Poire</option>
-    </select><br><br>
+<form method="POST" action="formulaire.php">
 
-    <label>Poids</label><br>
-    <input type="text" name="poids"><br><br>
+    <label for="fruit">Choisir un fruit</label>
 
-    <input type="submit" value="Afficher">
+    <select name="fruit" id="fruit">
+        <option value="pommes" <?php if($fruit_saisi == 'pommes') echo 'selected'; ?> >pommes</option>
+        <option value="poires" <?php if($fruit_saisi == 'poires') echo 'selected'; ?>>poires</option>
+        <option value="bananes" <?php if($fruit_saisi == 'bananes') echo 'selected'; ?>>bananes</option>
+        <option value="cerises" <?php if($fruit_saisi == 'cerises') echo 'selected'; ?>>cerises</option>
+    </select>
+    <br>
+
+    <label for="poids">poids</label>
+    <input type="text" name="poids" id="poids" value="<?php echo $poids_saisi; ?>">
+
+    <input type="submit" value="OK">
 
 </form>
+
+
+<?php
+
+// ici c pour afficher la phrase et le calcule et on lance le calcule que si les champs fruit et poids sont remplis.
+if( !empty($_POST['fruit']) && !empty($_POST['poids']) ) {
+    echo '<br>' . calcul($_POST['fruit'], $_POST['poids']) ;
+}
+else {
+    echo "je n'ai pas encore rempli le formulaire";
+}
+
+?>
+
+
+</body>
+</html>
+
