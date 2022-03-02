a9a8990dc5b2f3f022c1faa99045296c8a1c51ee
gaelle kerforne <stage.seeweb@gmail.com>
2022-03-02 14:21:56 +0100

    ajout du controle select
    Took 8 minutes

--- EvalPHP/ajoutAnnonce.php
+++ EvalPHP/ajoutAnnonce.php
@@ -12,12 +12,15 @@ if ($_POST) { //SI on valide le formulaire
         $_POST[$key] = htmlentities(addslashes($value));
     }
 
-    // !TODO faire la verif du select ---------------------------------------------
     if (empty($_POST['title']) || empty($_POST['description']) || empty($_POST['city']) || empty($_POST['price'])) {
 
         $error .= "<p>Veuillez renseigner les champs obligatoires</p>";
     }
 
+    if(isset($_POST['type']) != 'location' || $_POST['type'] != 'vente'){
+        $error .= "<p>Veuillez renseigner le type du biens</p>";
+        //debug($_POST);
+    }
     //--------------------- Code postal ------------------------
 
     if (!empty($_POST['postal_code'])) {
@@ -65,26 +68,26 @@ if ($_POST) { //SI on valide le formulaire
     <form action="" method="post">
         <?= "<div class='text-center mt-3 mb-3 bg-danger'>" . $error . "</div>";  //Affichage des messages d'erreurs ?>
 
-        <label for="title" class="form-label mt-3">Titre de l’annonce</label>
+        <label for="title" class="form-label mt-3">Titre de l’annonce *</label>
         <input type="text" name="title" id="title" class="form-control">
 
-        <label for="description" class="form-label mt-3">Description de l’annonce</label>
+        <label for="description" class="form-label mt-3">Description de l’annonce *</label>
         <input type="text" name="description" id="description" class="form-control">
 
-        <label for="postal_code" class="form-label mt-3">Code Postal</label>
+        <label for="postal_code" class="form-label mt-3">Code Postal *</label>
         <input type="number" name="postal_code" id="postal_code" class="form-control">
 
-        <label for="city" class="form-label mt-3">Ville</label>
+        <label for="city" class="form-label mt-3">Ville *</label>
         <input type="text" name="city" id="city" class="form-control">
 
         <!--        <label for="" class="form-label">Type d'annonce</label>-->
         <select class="form-select mt-3" aria-label="Type d'annonce" name="type">
-            <option selected>Votre type d'annonce</option>
+            <option selected>Votre type d'annonce *</option>
             <option value="location">Location</option>
             <option value="vente">Vente</option>
         </select>
 
-        <label for="price" class="form-label mt-3">Prix</label>
+        <label for="price" class="form-label mt-3">Prix *</label>
         <input type="number" name="price" id="price" class="form-control">
 
         <input type="submit" value=" Ajouter" class="btn btn-primary mt-3">
